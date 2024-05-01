a92988b7b43cd40a8924eb8a568a1d86528715c8
naga-sai1 <nagasairayapudi2000@gmail.com>
2024-05-01 12:59:48 +0000

    auth function

---
+++ src/components/ProtectedRoute/index.js
@@ -0,0 +1,13 @@
+// Write your JS code here
+import {Route, Redirect} from 'react-router-dom'
+import cookies from 'js-cookie'
+
+const ProtectedRoute = props => {
+  const token = cookies.get('jwt_token')
+  if (token === undefined) {
+    return <Redirect to="/login" />
+  }
+  return <Route {...props} />
+}
+
+export default ProtectedRoute
